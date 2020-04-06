29777deeb1b27cdc8816d496d8fd8d61bf35ad2b
unknown <arfanirizal22@gmail.com>
2020-04-06 15:34:15 +0700

    Ini Mata Kuliah Computer Vision

---
+++ Latihan dan Materi/Hello_word.py
@@ -0,0 +1,8 @@
+print ("Belajar Python di malasngoding.com")
+print ("Belajar Pemrograman di malasngoding.com")
+print ("Belajar di malasngoding.com")
+
+luas = 50
+lebar = 30
+hasil = luas * lebar
+print ("hasil ny"hasil)
