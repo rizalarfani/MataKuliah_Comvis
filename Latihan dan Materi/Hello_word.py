fa465e619151de0c3d249073b1d788fbde41e1a0
unknown <arfanirizal22@gmail.com>
2020-04-06 20:57:41 +0700

    Materi Image Prosessing dan Latihan Noise Renoval

--- Latihan dan Materi/Hello_word.py
+++
@@ -1,8 +0,0 @@
-print ("Belajar Python di malasngoding.com")
-print ("Belajar Pemrograman di malasngoding.com")
-print ("Belajar di malasngoding.com")
-
-luas = 50
-lebar = 30
-hasil = luas * lebar
-print ("hasil ny"hasil)
